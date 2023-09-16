41ea0902805d4b94525faa5bc203dc3cbf2ba734
Aidan <am.mcdonald00@gmail.com>
2023-09-16 12:12:13 +0800

    attempt with new src

--- src/components/Headshot.jsx
+++ src/components/Headshot.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Headshot() {
   return (
     <div className='headshot-div'>
-        <img src='..\src\assets\headshot1.png' className='headshot-img'/>
+        <img src='.\src\assets\headshot1.png' className='headshot-img'/>
     </div>
   )
 }
